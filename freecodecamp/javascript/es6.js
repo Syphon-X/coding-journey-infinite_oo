23e706ed5c139e1d6eee85d7e626ef44a30bb861
Ted Chow <142191365+Syphon-X@users.noreply.github.com>
2023-10-30 20:06:21 -0700

    Update es6.js

--- freecodecamp/javascript/es6.js
+++ freecodecamp/javascript/es6.js
@@ -14,4 +14,15 @@ function checkScope()
 }
 console.log(checkScope());
 
+// constants doesn't allow mutability through the variable identifier, but through element assignment
+
+const s = [5, 7, 2];
+function editInPlace() 
+{
+    s[0] = 2;
+    s[1] = 5;
+    s[2] = 7;
+}
+editInPlace();
+console.log(s);  // will return s as [2, 5, 7]
 
