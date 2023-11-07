56a8dc02d04c31b1a93adb9c864245fcbe17c7aa
Ted Chow <142191365+Syphon-X@users.noreply.github.com>
2023-11-06 19:36:49 -0800

    Update index.py

--- python/index.py
+++ python/index.py
@@ -3,3 +3,15 @@
 
 print('Hello World!')
 
+password checker #
+
+name = input('What is your name? ')
+password = input('Please enter your password: ')
+secret_password = '#' * len(password)
+characters = len(password)
+
+def password_check():
+    answer = print(f'Your name is {name}, your password: {secret_password} is {characters} characters long.')
+    return answer
+
+print(password_check())
