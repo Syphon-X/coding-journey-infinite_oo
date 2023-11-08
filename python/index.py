7a765dea309212d4d8bebf302b677430f43bb724
Ted Chow <142191365+Syphon-X@users.noreply.github.com>
2023-11-07 20:34:45 -0800

    Update index.py

--- python/index.py
+++ python/index.py
@@ -15,3 +15,15 @@ def password_check():
     return answer
 
 print(password_check())
+
+# list unpacking
+
+a, b, c, *other, d = [1, 2, 3, 4, 5, 6, 7, 8]
+
+print(a)
+print(b)
+print(c)
+print(other)
+print(d)
+
+
