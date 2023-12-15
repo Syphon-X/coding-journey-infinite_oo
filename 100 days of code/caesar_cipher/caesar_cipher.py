36c973b73e9726d38751a5a564447fee89de95b8
Ted Chow <142191365+Syphon-X@users.noreply.github.com>
2023-12-14 22:09:52 -0800

    Update caesar_cipher.py

--- 100 days of code/caesar_cipher/caesar_cipher.py
+++ 100 days of code/caesar_cipher/caesar_cipher.py
@@ -5,14 +5,11 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
-while True:
-    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-    if direction != 'encode' or 'decode':
-        print('Invalid direction, please type \'encode\' or \'decode\'.')
-        direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-    text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number:\n"))
-    break
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+    
 
 # def encrypt(plain_text, shift_amount):
 #     cipher_text = ""
